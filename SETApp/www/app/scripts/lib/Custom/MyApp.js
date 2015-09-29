ac359e7169e925b59ac40b1ae8833a26952e11ea
Tri Nguyen Minh <tringuyenminh92@gmail.com>
2015-09-29 18:49:41 +0700

    add modal loading, popover for quote selection navigations

--- SETApp/www/app/scripts/lib/Custom/MyApp.js
+++ SETApp/www/app/scripts/lib/Custom/MyApp.js
@@ -16,115 +16,14 @@
  *************************************************************/
 
 (function () {
-   
 
     "use strict";
 
      angular.module('GlobalModule', ['onsen', 'ngAnimate', 'ngRoute', 'ngSanitize', 'ngTouch']);
 
-    console.log('app starting', navigator.userAgent);
-  
-    // config settings flag, to indicate which kind of service 
-    // will be used: localservice, webapi service,...
-    angular.module("GlobalModule").configuration = {
-        useLocalData: true
-    };
-
-    //angular.module("GlobalModule").config(['$routeProvider', '$compileProvider',
-    //       function ($routeProvider, $compileProvider) {
-    //           $routeProvider
-    //               .when("/albums", {
-    //                   templateUrl: "app/views/albums.html"
-    //               })
-    //               .when("/album/edit/:albumId", {
-    //                   templateUrl: "app/views/editalbum.html"
-    //               })
-    //               .when("/album/:albumId", {
-    //                   templateUrl: "app/views/album.html"
-    //               })
-    //               .when("/artists", {
-    //                   templateUrl: "app/views/artists.html"
-    //               })
-    //               .when("/artist/:artistId", {
-    //                   templateUrl: "app/views/artist.html"
-    //               })
-    //               .when("/about", {
-    //                   templateUrl: "app/views/about.html"
-    //               })
-    //               .when("/options", {
-    //                   templateUrl: "app/views/options.html"
-    //               })
-    //               .otherwise({
-    //                   redirectTo: '/options'
-    //               });
-
-    //           // Fix issue for Windows Phone wanting to download files on urls with routed parameters
-    //           $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|ghttps?|ms-appx|x-wmapp0):/);
-    //       }
-    //])
-    //   .filter('linebreakFilter', function () {
-    //       return function (text) {
-    //           if (text !== undefined && text)
-    //               return text.replace(/\n/g, '<br />');
-    //           return text;
-    //       };
-    //   });
-
     angular.module("GlobalModule").factory("shareService", shareService);
 
 })();
-
-//Service to call Modal
-modalService.$inject = ['$modal'];
-function modalService($modal) {
-
-    var serviceObject = {};
-    serviceObject.ShowModal = function (funcOk, funcCancel, passData) {
-
-        var modalInstance = $modal.open({
-            templateUrl: passData.Template || 'gridModal.html',
-            backdrop: 'static',
-            keyboard: false,
-            controller: passData.Controller || 'gridModalController',
-            size: passData.Size || 'lg',
-            resolve: {
-                data: function () { return passData; }
-            }
-        });
-
-        modalInstance.result.then(funcOk, funcCancel);
-    };
-
-    return serviceObject;
-}
-
-
-//Service to show notification message
-notifyService.$inject = ['$rootScope'];
-function notifyService($rootScope) {
-
-    'use strict';
-
-    $rootScope.queue = [];
-
-    var serviceObject = {};
-
-    serviceObject.add = function (item) {
-        $rootScope.queue.push(item);
-        setTimeout(function () {
-            // remove the alert after 2000 ms
-            $('.alerts .alert').eq(0).remove();
-            $rootScope.queue.shift();
-        }, 4000);
-    },
-
-    serviceObject.pop = function () {
-        $rootScope.queue.pop();
-    };
-
-    return serviceObject;
-
-}
 
 
 //Share data service
---
+++ SETApp/www/app/scripts/lib/Custom/quoteController.js
@@ -0,0 +1,31 @@
+﻿
+/*************************************************************
+ *
+ *	Created By: Nguyen Minh Tri - UR81HC
+ *  Created Date: 29-09-2015
+ *	Description: add comment header
+ *	
+ *	Modified By: Nguyen Minh Tri - UR81HC
+ *	Modified Date: 29-09-2015
+ *	Description:
+ *
+ *************************************************************/
+angular.module("GlobalModule").controller("quoteController", quoteController);
+quoteController.$inject = ['$scope'];
+
+function quoteController($scope) {
+
+    "use strict";
+
+    //show popover for navigation to Header, Summary,  BOMs
+    $scope.showActionsToSelectedQuote = function ($event) {
+        console.log($event.originalTarget);
+        ons.createPopover('quoteNavigationOptions.html').then(function (popover) {
+            $scope.popover = popover;
+            $scope.popover.show($event.originalTarget);
+        });
+
+    };
+
+    $scope.options = ['BOMs', 'Summary', 'Header'];
+}
