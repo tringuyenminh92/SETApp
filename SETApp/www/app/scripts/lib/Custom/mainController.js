7a4b4f88a3001c9a2b2ffdd283b215ca822ee394
Tri Nguyen <tringuyenminh92@gmail.com>
2015-10-21 23:54:32 +0700

    update images for backeground, init func for backstretch

--- SETApp/www/app/scripts/lib/Custom/mainController.js
+++ SETApp/www/app/scripts/lib/Custom/mainController.js
@@ -19,5 +19,13 @@ function mainController($scope) {
 
     $scope.isLogged = false;
 
+    $scope.initBackground = function () {
+        //$(".page__background").backstretch("app/images/backgroundLogin.jpg");
+        $(".page__background").backstretch(["app/images/w02.jpg", "app/images/w01.jpg", "app/images/w03.jpg", "app/images/w04.jpg"], { speed: 350 });
+    };
+
+    $scope.login = function () {
+        $scope.isLogged = !$scope.isLogged;
+    };
 
 }
